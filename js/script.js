d5ab0f96e0d83341c38ed84dbf07449a24d90b3e
vitorfviana <vitor.viana@sptech.school>
2022-06-04 23:55:37 -0300

    Landing Page finalizada

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-const header = document.getElementById("header");
-
-window.onscroll(mudaHeader());
-
-function mudaHeader() {
-    if (window.pageYOffset > 200){
-        header.style.backgroundColor = '#36114d'
-        header.style.transition = 'all 0.4s ease-in-out'
-    }
-    else {
-        header.style.backgroundColor = '#000000a0'
-        
-    }
-}
-
- window.onscroll = () => {
-  window.pageYOffset > 200
-    ? (header.style.backgroundColor = 'var(--ui-black-02)') &&
-      (header.style.transition = 'all 0.4s ease-in-out') &&
-      (header.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.5)')
-    : (header.style.backgroundColor = 'transparent') &&
-      (header.style.boxShadow = 'none');
-};
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("#header_lp");
+  header.classList.toggle("sticky", window.scrollY > 50)
+})
